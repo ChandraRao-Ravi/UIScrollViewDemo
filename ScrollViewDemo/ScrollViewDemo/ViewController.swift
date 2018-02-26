d8fefee8da9d2be9a92ab7566e4e3cf72949bc22
Ritesh Gupta <riteshgupta@Riteshs-MacBook-Pro.local>
2018-02-27 02:32:00 +0530

    Initial Commit

---
+++ ScrollViewDemo/ScrollViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewDemo
+//
+//  Created by Ritesh Gupta on 27/02/18.
+//  Copyright © 2018 Chandra Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
